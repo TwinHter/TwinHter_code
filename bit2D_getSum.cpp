df2d8b4a4a68742480bff1f4e2d3028cc2c508ae
TwinHter <104690708+TwinHter@users.noreply.github.com>
2024-01-06 16:50:00 +0700

    Update bit2D_getSum.cpp

--- bit2D_getSum.cpp
+++ bit2D_getSum.cpp
@@ -1,30 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
- 
-typedef long long ll;
-typedef long double ld;
-typedef pair<ll, ll> pll;
-typedef pair<int, int> pii;
-typedef pair<long double, long double> pdd;
-typedef vector<vector<ll> > matrix;
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define popb pop_back
-#define pf push_front
-#define popf pop_front
-template <class T> inline T sqr(T x) { return x * x; };
-template <class T> inline bool maximize(T& a, T b) { return (a < b) ? a = b, true : false; }
-template <class T> inline bool minimize(T& a, T b) { return (a > b) ? a = b, true : false; }
- 
-string lowcase = "abcdefghijklmnopqrstuvwxyz";
-string upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-string number = "0123456789";
-int dx[] = {0, 0, -1, 1};
-int dy[] = {-1, 1, 0, 0};
-const ll pd = 998244353;
-const ll oo = 9e18 + 1;
 const int MX = 2000010, LIM = 20;
 ll bit[1030][1030], cur[1030][1030];
 int n;
