50c98bb2e37ea3bd90b72f951d6ca5f60c834631
TwinHter <104690708+TwinHter@users.noreply.github.com>
2024-01-06 16:51:22 +0700

    Update merge_segment_tree.cpp

--- merge_segment_tree.cpp
+++ merge_segment_tree.cpp
@@ -1,34 +1,6 @@
 //https://www.spoj.com/problems/MKTHNUM/
 // #pragma GCC optimize("O3,unroll-loops")
 // #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
-#include<bits/stdc++.h>
-using namespace std;
- 
-typedef long long ll;
-typedef long double ld;
-typedef pair<ll, ll> pll;
-typedef pair<int, int> pii;
-typedef pair<long double, long double> pdd;
-typedef vector<vector<ll> > matrix;
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define popb pop_back
-#define pf push_front
-#define popf pop_front
-#define all(x) x.begin(),x.end()
-template <class T> inline T sqr(T x) { return x * x; };
-template <class T> inline bool maximize(T& a, T b) { return (a < b) ? a = b, true : false; }
-template <class T> inline bool minimize(T& a, T b) { return (a > b) ? a = b, true : false; }
- 
-string lowcase = "abcdefghijklmnopqrstuvwxyz";
-string upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-string number = "0123456789";
-int dx[] = {0, 0, -1, 1};
-int dy[] = {-1, 1, 0, 0};
-const ll pd = 998244353;
-const ll oo = 3e18 + 1;
 const int MX = 200100, LIM = 25, block = 320;
 int n, q, a[MX];
 vector<int> it[MX*4];
@@ -66,11 +38,4 @@ void TwinHter() {
         }
         cout << ans << '\n';
     }
-
-}
-int main (){
-    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    //freopen("cf.inp", "r", stdin); //freopen("cf.out", "w", stdout);
-    int tests = 1; //cin >> tests; 
-    while(tests--) TwinHter();
 }
