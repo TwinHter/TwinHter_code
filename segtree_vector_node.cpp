4d6476e4343b13a37e639fed0556b97715ae784e
TwinHter <104690708+TwinHter@users.noreply.github.com>
2024-01-06 16:49:36 +0700

    Update segtree_vector_node.cpp

--- segtree_vector_node.cpp
+++ segtree_vector_node.cpp
@@ -1,32 +1,4 @@
 //https://www.spoj.com/problems/KQUERYO/
-#include<bits/stdc++.h>
-using namespace std;
- 
-typedef long long ll;
-typedef long double ld;
-typedef pair<ll, ll> pll;
-typedef pair<int, int> pii;
-typedef pair<long double, long double> pdd;
-typedef vector<vector<ll> > matrix;
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define popb pop_back
-#define pf push_front
-#define popf pop_front
-#define all(x) x.begin(),x.end()
-template <class T> inline T sqr(T x) { return x * x; };
-template <class T> inline bool maximize(T& a, T b) { return (a < b) ? a = b, true : false; }
-template <class T> inline bool minimize(T& a, T b) { return (a > b) ? a = b, true : false; }
- 
-string lowcase = "abcdefghijklmnopqrstuvwxyz";
-string upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-string number = "0123456789";
-int dx[] = {0, 0, -1, 1};
-int dy[] = {-1, 1, 0, 0};
-const ll pd = 998244353;
-const ll oo = 9e18 + 1;
 const int MX = 300010, LIM = 55000;
 vector<int> seg[MX*4];
 int n;
