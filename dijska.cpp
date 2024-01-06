236b8ee1b637cf8ed8d5a4d8342bf505d65e24c6
TwinHter <104690708+TwinHter@users.noreply.github.com>
2024-01-06 16:50:14 +0700

    Update dijska.cpp

--- dijska.cpp
+++ dijska.cpp
@@ -1,33 +1,4 @@
 // #pragma GCC optimize("Ofast")
-#include<bits/stdc++.h>
-using namespace std;
- 
-typedef long long ll;
-typedef long double ld;
-typedef pair<ll, ll> pll;
-typedef pair<int, int> pii;
-typedef pair<long double, long double> pdd;
-typedef pair<ll, int> pli;
-typedef vector<vector<ll> > matrix;
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define popb pop_back
-#define pf push_front
-#define popf pop_front
-#define all(x) x.begin(),x.end()
-template <class T> inline T sqr(T x) { return x * x; };
-template <class T> inline bool maximize(T& a, T b) { return (a < b) ? a = b, true : false; }
-template <class T> inline bool minimize(T& a, T b) { return (a > b) ? a = b, true : false; }
- 
-string lowcase = "abcdefghijklmnopqrstuvwxyz";
-string upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-string number = "0123456789";
-int dx[] = {0, 0, -1, 1};
-int dy[] = {-1, 1, 0, 0};
-const ll pd = 1e9+7; //998244353;
-const ll oo = 3e18 + 1;
 const int MX = 100510, Lim = 60, base = 137;
 vector<pii> a[MX];
 ll dp[MX], pre[MX];
