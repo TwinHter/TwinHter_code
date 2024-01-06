3499066d1970642161d1325d598053d4b4894086
TwinHter <104690708+TwinHter@users.noreply.github.com>
2024-01-06 16:51:47 +0700

    Update matrix_mul.cpp

--- matrix_mul.cpp
+++ matrix_mul.cpp
@@ -1,31 +1,4 @@
 //https://codeforces.com/contest/1182/problem/E
-#include<bits/stdc++.h>
-using namespace std;
-
-typedef long long ll;
-typedef long double ld;
-typedef pair<ll, ll> pll;
-typedef pair<int, int> pii;
-typedef pair<long double, long double> pdd;
-typedef vector<vector<ll> > matrix;
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define popb pop_back
-#define pf push_front
-#define popf pop_front
-template <class T> inline T sqr(T x) { return x * x; };
-template <class T> inline bool maximize(T& a, T b) { return (a < b) ? a = b, true : false; }
-template <class T> inline bool minimize(T& a, T b) { return (a > b) ? a = b, true : false; }
-
-string lowcase = "abcdefghijklmnopqrstuvwxyz";
-string upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-string number = "0123456789";
-int dx[] = {0, 0, -1, 1};
-int dy[] = {-1, 1, 0, 0};
-const ll pd = 1e9 + 7;
-ll oo = 9e18 + 1;
 const int MX = 500100;
 ll n, ans = 1;
 matrix base;
@@ -66,10 +39,4 @@ void TwinHter() {
     cin >> n ;
     base = {{0, 1, 0}, {0, 0, 1}, {1, 1, 1}};
     base = pw_matrix(base, n-3);
-}
-int main (){
-    ios_base :: sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    //freopen("cf.inp", "r", stdin); //freopen("cf.out", "w", stdout);
-    int tests = 1; //cin >> tests; 
-    while(tests--) TwinHter();
 }
