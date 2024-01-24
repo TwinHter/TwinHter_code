199c342a3a0eb82b27ce828fa6666619a7af5484
TwinHter <104690708+TwinHter@users.noreply.github.com>
2024-01-24 17:50:57 +0700

    Update khớp và cầu.cpp

--- khớp và cầu.cpp
+++ khớp và cầu.cpp
@@ -1,33 +1,5 @@
-// #pragma GCC optimize("O3,unroll-loops")
-// #pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
-#include<bits/stdc++.h>
-using namespace std;
- 
-typedef long long ll;
-typedef long double ld;
-typedef pair<ll, ll> pll;
-typedef pair<int, int> pii;
-typedef pair<long double, long double> pdd;
-typedef vector<vector<ll> > matrix;
-#define fi first
-#define se second
-#define mp make_pair
-#define pb push_back
-#define popb pop_back
-#define pf push_front
-#define popf pop_front
-#define all(x) x.begin(),x.end()
-template <class T> inline T sqr(T x) { return x * x; };
-template <class T> inline bool maximize(T& a, T b) { return (a < b) ? a = b, true : false; }
-template <class T> inline bool minimize(T& a, T b) { return (a > b) ? a = b, true : false; }
- 
-string lowcase = "abcdefghijklmnopqrstuvwxyz";
-string upcase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-string number = "0123456789";
 int dx[] = {0, 0, -1, 1};
 int dy[] = {-1, 1, 0, 0};
-const ll pd = 998244353;
-const ll oo = 3e18 + 1;
 const int MX = 200100, Lim = 20, block = 320;
 int n, m, dtime = 0, num[MX], low[MX];
 vector<int> a[MX], khop;
